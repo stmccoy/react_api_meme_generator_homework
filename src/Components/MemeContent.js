bec57d739f46802302348b71c58b93aba5792bb3
crispymccoy145 <samspam19941@gmail.com>
2021-05-14 15:29:22 +0100

    initial file structure set up

---
+++ src/Components/MemeContent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MemeContent(){
+    return(
+        <p>Please Work</p>
+    );
+};
+
+export default MemeContent;
---
+++ src/Container/MemeContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MemeContent from '../Components/MemeContent'
+
+
+function MemeContainer(){
+
+    return(
+        <MemeContent />
+    );
+
+};
+
+export default MemeContainer;
